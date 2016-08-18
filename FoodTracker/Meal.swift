7636e7498a1db8f7c29b7ff10997de33d85350f4
Randall Ball <16randall@gmail.com>
2016-08-17 21:47:41 -0600

    app is done

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,67 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Randall Ball on 7/30/16.
+//  Copyright © 2016 Ball Enterprise. All rights reserved.
+//
+
+import UIKit
+
+class Meal: NSObject, NSCoding {
+    
+    //MARK:Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    //MARK: Archiving Paths
+    
+    static let DocumentsDirectory = NSFileManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.URLByAppendingPathComponent("meals")
+    
+    //MARK: Types
+    
+    struct PropertyKey {
+        static let nameKey = "name"
+        static let photoKey = "photo"
+        static let ratingKey = "rating"
+    }
+    
+    //MARK: Initializer
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        //Declare properties
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        super.init()
+        
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+    }
+    
+    //Mark: NSCoding
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(name, forKey: PropertyKey.nameKey)
+        aCoder.encodeObject(photo, forKey: PropertyKey.photoKey)
+        aCoder.encodeInteger(rating, forKey: PropertyKey.ratingKey)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        let name = aDecoder.decodeObjectForKey(PropertyKey.nameKey) as! String
+        
+        //photo is optional, so use conditional cast
+        let photo = aDecoder.decodeObjectForKey(PropertyKey.photoKey) as? UIImage
+        
+        let rating = aDecoder.decodeIntegerForKey(PropertyKey.ratingKey)
+        
+        // Must call designated initializer.
+        self.init(name: name, photo: photo, rating: rating)
+    }
+    
+}
---
+++ MealTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Randall Ball on 7/31/16.
+//  Copyright © 2016 Ball Enterprise. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    //MARK: Properties
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
